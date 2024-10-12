e091a93224d57090932bdf9bec844c452d3cba1e
Alexandre Ramos <132162311+progr-amador@users.noreply.github.com>
2024-10-12 19:48:25 +0100

    penso que acabei o llopen, vê o que achas, fui pegando pedaços do write.c e read.c

--- Project1/README.md
+++
@@ -1,5 +0,0 @@
-# RCOM-Hub
-Welcome to the RCOM-Hub, a repository containing our projects for the Computer Networks (Redes de Computadores) course. Here, you'll find implementations and experiments focusing on data communication and networking protocols.
-
-## Projects
-### 1. Project 1: [Data Link Protocol]
--- Project1/src/link_layer.c
+++ Project1/src/link_layer.c
@@ -4,6 +4,17 @@
 #include "link_layer.h"
 #include "serial_port.h"
 
+// achas que posso passar esta função para aqui? outra alternativa seria criar mesmo um alarm.c alarm.h, é que caso contrário não temos o alarm handler
+int alarmEnabled = FALSE;
+int alarmCount = 0;
+
+void alarmHandler(int signal) {
+    alarmEnabled = FALSE;
+    alarmCount++;
+
+    printf("Alarm #%d\n", alarmCount);
+}
+
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
 #define BUF_SIZE 256
@@ -12,25 +23,37 @@
 ////////////////////////////////////////////////
 int llopen(LinkLayer connectionParameters)
 {
-    int alarmEnabled = FALSE;
-    int alarmCount = 0;
+    (void)signal(SIGALRM, alarmHandler);
+    unsigned char buf[BUF_SIZE] = {0};
 
-    if (openSerialPort(connectionParameters.serialPort,
-                       connectionParameters.baudRate) < 0 && alarmCount == 0)
+    int fd = openSerialPort(connectionParameters.serialPort, connectionParameters.baudRate);
+    if (fd < 0) // && alarmCount == 0) acabamos de definir o alarmCount como 0 na linha anterior
     {
         return -1;
     }
 
     State state = START_S;
 
-    unsigned char buf[BUF_SIZE] = {0};
     while (state != STOP_S) {
 
         if (alarmEnabled == FALSE && connectionParameters.role == LlTx)
         {
+            buf[0] = 0x7E;
+            buf[1] = 0x03;
+            buf[2] = 0x03;
+            buf[3] = buf[1] ^ buf[2];
+            buf[4] = 0x7E;
+
+            int bytes = write(fd, buf, 5);
+            printf("%d bytes written\n", bytes);
+
             alarm(connectionParameters.timeout);
             alarmEnabled = TRUE;
         }
+
+        int bytes = read(fd, buf, 1);
+        if (!bytes) continue;
+        printf("receivedByte = 0x%02X\n", buf[0]);
 
         switch (state) {
             case START_S:
@@ -76,6 +99,7 @@ int llopen(LinkLayer connectionParameters)
             case BCC_OK_S:
                 if (buf[0] == FLAG) {
                     state = STOP_S;
+                    printf("sucesso!\n");
                     break;
                 }
                 else {
@@ -91,7 +115,17 @@ int llopen(LinkLayer connectionParameters)
             break;
         }
     }
-    // TODO 
+
+    if(connectionParameters.role == LlRx) {
+        buf[0] = 0x7E;
+        buf[1] = 0x01;
+        buf[2] = 0x07;
+        buf[3] = buf[1] ^ buf[2];
+        buf[4] = 0x7E;
+
+        int bytes = write(fd, buf, BUF_SIZE); // Send all 5 bytes including
+        printf("%d bytes written\n", bytes);
+    } 
 
     return 1;
 }
